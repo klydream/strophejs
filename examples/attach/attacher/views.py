bc338ab03049753b29f3e018b999ec54cbef4c81
星海观尘 <kly_dream@163.com>
2018-05-10 06:56:18 +0800

    Add files via upload

--- examples/attach/attacher/views.py
+++ examples/attach/attacher/views.py
@@ -8,11 +8,11 @@ def index(request):
     bc = BOSHClient(JABBERID, PASSWORD, BOSH_SERVICE)
     bc.startSessionAndAuth()
 
-    t = loader.get_template("attacher/index.html")
-    c = Context({
+    t = loader.get_template("index.html")
+    c = {
 	    'jid': bc.jabberid.full(),
 	    'sid': bc.sid,
 	    'rid': bc.rid,
-    })
+    }
 
     return HttpResponse(t.render(c))
